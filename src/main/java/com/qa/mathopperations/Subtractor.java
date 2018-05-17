b714d1e343b5d9c62e252c092a3151c8c9ad7433
anmaraq <anmar_aq@hotmail.co.uk>
2018-05-17 11:44:28 +0100

    Subtract feature

---
+++ src/main/java/com/qa/mathopperations/Subtractor.java
@@ -0,0 +1,12 @@
+package com.qa.mathopperations;
+
+public class Subtractor {
+
+	public int subtract(int num1, int num2) {
+		// TODO Auto-generated method stub
+		
+		
+		return num1-num2;
+	}
+
+}
